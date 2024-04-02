3a2b1f7b1f419e5b0182bdd274dd75b982e57954
oleh_teslenko <teslenkooleg2017@gmail.com>
2024-04-02 13:53:55 +0300

    Add gridstack logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {GridStackOptions, GridStackWidget} from "gridstack";
+import {GridstackModule, nodesCB} from "gridstack/dist/angular";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GridstackModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-interactive-dashboard';
+  public gridOptions: GridStackOptions = { margin: 5 }
+  public items: GridStackWidget[] = [
+    {x:0, y:0, w:4, id:'1'},
+    {x:1, y:0, w:3, id:'2'},
+    {x:0, y:1, h:4, w:4, id:'3'},
+    {x:3, y:1, w:3, id:'4'},
+    {x:0, y:2, w:2, id:'5'},
+  ];
+
+  public onChange(data: nodesCB) {
+    console.log('change ', data.nodes.length > 1 ? data.nodes : data.nodes[0]);
+  }
+
 }
